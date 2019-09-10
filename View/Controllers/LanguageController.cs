e922610201b6eb0b7f328b486afc282391b28a83
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2019-09-10 10:42:47 -0300

    Initial commit

---
+++ View/Controllers/LanguageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class LanguageController : Controller
+    {
+        
+        public ActionResult Change(String lang)
+        {
+            if (lang != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lang);
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(lang);
+                HttpCookie cookie = new HttpCookie("Language");
+                cookie.Value = lang;
+                Response.Cookies.Add(cookie);
+            }
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+    }
+}
